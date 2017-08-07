a64053b84c29617b53648a160d1cc7ff6abae07d
zmigsod <tingping@TingPingdeMacBook-Pro-2.local>
2017-08-07 16:26:49 +0800

    支持发送广播消息

--- WebsocketServer.php
+++ WebsocketServer.php
@@ -6,9 +6,11 @@ class WebsocketServer
 
     public static $uid = [];
 
-    public static $toUid = [];
+    public static $uidInfo = [];
 
     private $close;
+
+    private $admin = false;
 
     public static $message = [];
 
@@ -38,27 +40,36 @@ class WebsocketServer
     public function onOpen($server, $req)
     {
         //先绑定uid与fd
-        $this->bindUid($req->get['id'], $req->fd);
+        $this->bindUid(['id' => $req->get['id'], 'name' => ''], $req->fd);
         $this->showData();
     }
 
     public function onMessage($server, $frame)
     {
-        if (!empty($frame->data)) {
+        if (!empty($server)) {
             $result = json_decode($frame->data, true);
-            //根据前端传递的toid获取要发送到此toid绑定的fd
-            $sendToFd = isset(self::$uid[$result['toid']]) ? self::$uid[$result['toid']] : false;
-            if($sendToFd === false) {
-                $server->push($frame->fd, json_encode(['toid' => 'system', 'content'=> '对方不在线，请重试']));
+            if(self::$uidInfo[$this->getUid($frame->fd)]['role'] == 'admin') {
+                foreach (self::$uid as $v) {
+                    if($v != $frame->fd) {
+                        $server->push($v, json_encode(['toid' => 'system', 'content'=> 'System Broadcast: '.$result['content']]));
+                    }
+                }
+                $server->push($frame->fd, json_encode(['toid' => 'system', 'content'=> '广播发送成功']));
             }else{
-                if($sendToFd == $frame->fd) {
-                    $server->push($frame->fd, json_encode(['toid' => 'system', 'content'=> '不能给自己发送消息哦！']));
+                //根据前端传递的toid获取要发送到此toid绑定的fd
+                $sendToFd = isset(self::$uid[$result['toid']]) ? self::$uid[$result['toid']] : false;
+                if($sendToFd === false) {
+                    $server->push($frame->fd, json_encode(['toid' => 'system', 'content'=> '对方不在线，请重试']));
                 }else{
-                    if($result['content']) {
-                        //发送到此toid的fd中
-                        $server->push($sendToFd, json_encode(['toid' => 'id = '. $this->getUid($frame->fd), 'content' => $result['content']]));
+                    if($sendToFd == $frame->fd) {
+                        $server->push($frame->fd, json_encode(['toid' => 'system', 'content'=> '不能给自己发送消息哦！']));
                     }else{
-                        $server->push($frame->fd, json_encode(['toid' => 'system', 'content'=> '发送不能为空']));
+                        if($result['content']) {
+                            //发送到此toid的fd中
+                            $server->push($sendToFd, json_encode(['toid' => 'id = '. $this->getUid($frame->fd), 'content' => $result['content']]));
+                        }else{
+                            $server->push($frame->fd, json_encode(['toid' => 'system', 'content'=> '发送不能为空']));
+                        }
                     }
                 }
             }
@@ -79,9 +90,14 @@ class WebsocketServer
         $this->_server->close($fd, $message);
     }
 
-    public function bindUid($uid, $fd)
+    public function bindUid($uidInfo, $fd)
     {
-        self::$uid[$uid] = $fd;
+        $isAdmin = $this->checkAdmin($uidInfo['id']);
+        self::$uid[$uidInfo['id']] = $fd;
+        self::$uidInfo[$uidInfo['id']] = [
+            'role' => $isAdmin,
+            'name' => isset($uidInfo['name']) ? $uidInfo['name'] : rand(100000, 999999),
+        ];
     }
 
     public function getUid($fd)
@@ -99,7 +115,17 @@ class WebsocketServer
         $result = array_search($fd, self::$uid);
         if($result !== false) {
             unset(self::$uid[$result]);
+            unset(self::$uidInfo[$result]);
         }
+    }
+
+    public function checkAdmin($userId)
+    {
+        if($userId == '838881690') {
+            echo '管理员: 838881690登录 '.PHP_EOL;
+            return 'admin';
+        }
+        return 'user';
     }
 }
 
