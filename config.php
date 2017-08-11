1a062a70a0064f5eb1a04b8c70cbcead6a0bde0f
zmigsod <tingping@TingPingdeMacBook-Pro-2.local>
2017-08-11 10:55:22 +0800

    Ignore file

--- config.php
+++ config.php
@@ -11,7 +11,8 @@ $config = [
         'worker_num' => 1, //worker进程设置为1，这样就能将fd与用户id进行唯一绑定，不然多进程下无法判断用户是否绑定
         'heartbeat_check_interval' => 60,
         'heartbeat_idle_time' => 125,
-        'package_eof' => '\r\n\r\n'
+        'daemonize' => 1,
+        'log_file' => __DIR__.'/swoole.log'
     ]
 ];
 ?>
