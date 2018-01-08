3b2e56c28a9f9e2027ed05cb71ca3133e9ec02c0
zmisgod <starzmisgod@gmail.com>
2018-01-08 18:12:44 +0800

    New

---
+++ config.php
@@ -0,0 +1,18 @@
+<?php
+$config = [
+    'base' => [
+        'host' => 'im.zmis.me',
+        'port' => 9205
+    ],
+    'manage' => [
+        'adminID' => '838881690'
+    ],
+    'swoole' => [
+        'worker_num' => 1, //worker进程设置为1，这样就能将fd与用户id进行唯一绑定，不然多进程下无法判断用户是否绑定
+        'heartbeat_check_interval' => 60,
+        'heartbeat_idle_time' => 125,
+        'daemonize' => 1,
+        'log_file' => __DIR__.'/swoole.log'
+    ]
+];
+?>
