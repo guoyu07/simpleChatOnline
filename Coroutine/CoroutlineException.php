335add073608302ea1677fbb0a2a8b52287760f4
zmisgod <starzmisgod@gmail.com>
2018-01-08 18:12:02 +0800

    Delete useless files

--- Coroutine/CoroutlineException.php
+++
@@ -1,4 +0,0 @@
-<?php
-namespace SimpleChatOnline\Coroutine;
-
-class CoroutlineException extends \Exception {}
--- Utility.php
+++
@@ -1,6 +0,0 @@
-<?php
-function sendJson($code = 200, $msg = '', $data = [])
-{
-    header('Content-type: text/json; charset = utf-8');
-    echo json_encode(['code' => $code, 'msg' => $msg, 'data' => $data]);
-}
